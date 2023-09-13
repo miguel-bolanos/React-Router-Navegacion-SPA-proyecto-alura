8d6a5abcca4d35f8b61f8213ab8d4ee5ca768a05
miguel-portatil <miguel.bol3456@gmail.com>
2023-09-13 10:40:08 -0500

    se inicia proyecto de alura llamado navegacion en una SPA se agregar paquete de react-router-dom para los path

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import './assets/css/base/base.css';
+import './assets/css/componentes/card.css'
+import Home from './pages/Home'
+import Sobre from './pages/Sobre'
+import { BrowserRouter as Router , Routes, Route } from 'react-router-dom';
+
+
+function App() {
+
+  const location = window.location.pathname
+  
+
+  
+  return (
+    <Router>
+     <Routes>
+        <Route path='/' element= {<Home/>}/>
+        <Route path='/sobre' element= {<Sobre/>}/>
+     </Routes>
+    </Router>
+  );
+}
+
+export default App;
