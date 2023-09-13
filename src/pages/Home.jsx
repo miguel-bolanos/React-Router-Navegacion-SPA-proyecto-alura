3064ec08c3d3ff42aa21c7f76dad6c7a444dbe5a
miguel-portatil <miguel.bol3456@gmail.com>
2023-09-13 12:52:24 -0500

    se agrego la lista de categorias en el cual son dos botones y se compartio en el elemento Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,15 @@
 import ListPosts from "../components/ListPosts"
+import ListCategories from "../components/ListCategories"
+
 
 const Home = () => {
   return (
     <main>
       <div className="container">
         <h2 className="title-page">Pet noticias</h2>
+        <ListCategories/>
         <ListPosts url={'/posts'}/>
+        
       </div>
     </main>
   )
